ac604e20f5980e77e736a41f2b5c5d8479be7ebe
adityarohman <adityarhmaan@gmail.com>
2024-12-17 22:14:50 +0700

    chore: setup flavor (ios)

---
+++ lib/main_development.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:solve_ai/main.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const SolveAIApp());
+}
